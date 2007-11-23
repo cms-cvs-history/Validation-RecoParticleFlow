0256152b10c74a083ff8422ba9dae7e822f91070
punz <sha1-be547d55cdf5572b1fe3f43af7c9a545955871a9@cern.ch>
2007-11-23 14:42:32 +0000

    restructured benchmarking and validation framework - initial commit

--- test/val.C
+++ test/val.C
@@ -3,16 +3,16 @@
 // library, which contains the ROOT interface
 
 gSystem->Load("libFWCoreFWLite.so");
-gSystem->Load("libRecoParticleFlowPFVal.so");
+gSystem->Load("libRecoParticleFlowBenchmark.so");
 AutoLibraryLoader::enable();
 gSystem->Load("libCintex.so");
 
 //ROOT::Cintex::Cintex::Enable();
 
 // create a PFRootEventManager
-FWL_PFEtVal em;
+PFBenchmarkAlgo my_obj;
 
 
 // look for ECAL rechit with maximum energy
-em.readData();
+
 }
