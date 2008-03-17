da3f28efc57db7c525e838c6d28f905229442bba
punz <sha1-be547d55cdf5572b1fe3f43af7c9a545955871a9@cern.ch>
2008-03-17 22:47:24 +0000

    initial import of WebScripts for the Validationframework

---
+++ test/ValidationFramework/WebScripts/compatibility_deltaPhivsEta.php
@@ -0,0 +1,94 @@
+<html>
+<head>
+  <title>Compatibility vs eta-ring</title>
+  <meta content="Thomas Punz & Ronald Remmington" name="author">
+  <script type="text/javascript">
+
+    function tn_l(bin) {
+      clear_r()
+      document.getElementById("thumblink_l").href = "DeltaPhivsEta/DeltaPhivsEtaCompare_etBin" + bin + ".gif"
+      document.getElementById("thumb_l").src = "DeltaPhivsEta/DeltaPhivsEtaCompare_etBin" + bin + ".gif"
+      document.getElementById("thumb_l").width = "250"
+      document.getElementById("thumb_l").height = "200"
+      document.getElementById("thumb_l").border = "1"
+    }
+
+    function tn_r(bin) {
+      clear_l()
+      document.getElementById("thumblink_r").href = "DeltaPhivsEta/DeltaPhivsEtaCompare_etBin" + bin + ".gif"
+      document.getElementById("thumb_r").src = "DeltaPhivsEta/DeltaPhivsEtaCompare_etBin" + bin + ".gif"
+      document.getElementById("thumb_r").width = "250"
+      document.getElementById("thumb_r").height = "200"
+      document.getElementById("thumb_r").border = "1"
+    }
+
+    function clear_l() {
+      document.getElementById("thumblink_l").href = "#"
+      document.getElementById("thumb_l").src = ""
+      document.getElementById("thumb_l").width = "0"
+      document.getElementById("thumb_l").height = "0"
+      document.getElementById("thumb_l").border = "0"
+    }
+
+    function clear_r() {
+      document.getElementById("thumblink_r").href = "#"
+      document.getElementById("thumb_r").src = "" 
+      document.getElementById("thumb_r").width = "0"
+      document.getElementById("thumb_r").height = "0"
+      document.getElementById("thumb_r").border = "0"
+    }
+
+    function clear_all() {
+      clear_l()
+      clear_r()
+    }
+
+  </script>
+</head>
+<body>
+<style type="text/css">
+#thumb_dl {position: absolute; position: fixed; left: 220px; top: 80px; text-align: center;}
+#thumb_dr {position: absolute; position: fixed; left: 600px; top: 80px; text-align: center;}
+
+</style>
+<!-- added for IE compatibility
+<script language="JScript">
+if (document.recalc && document.body.attachEvent) {
+  theDiv.style.setExpression("top", "document.body.scrollTop + 150");
+  document.body.onscroll = function() { document.recalc(true) };
+}
+</script> -->
+<!-- <div style="text-align: center;"><img src="Results_vs_etaRing.gif" usemap="#Results_vs_etaRing" alt="" style="border-style: none; width: 996px; height: 472px;"> -->
+<div id="main_d">
+<img src="DeltaPhivsEtadET_Score_vs_et.gif" usemap="#results" alt="" style="border-style: none;">
+<map id="#results" name="results" onMouseOut="Clear_all()">
+
+<?
+
+	$xpos=159.5;
+	$ypos=164.757;
+	for($i=1;$i<=100;$i++)
+	{
+		echo "<area shape=rect coords=".($xpos+($i-1)*7.36).",410,".($ypos+($i-1)*7.36).",39\n";
+ 		if($i<10)
+ 		{
+ 			echo "href=DeltaPhivsEta/DeltaPhivsEtaCompare_etBin000".$i.".gif\n";
+ 			echo "onMouseOver=tn_r('000".$i."')>\n";
+ 		}
+ 		else if($i<100)
+ 		{
+ 			echo "href=DeltaPhivsEta/DeltaPhivsEtaCompare_etBin00".$i.".gif\n";
+ 			echo "onMouseOver=tn_r('00".$i."')>\n";
+ 		}
+	}
+?>
+ 
+  </map>
+</div>
+
+<div id="All bins"><img src=DeltaPhivsEtadEt_Allbins.gif></div>
+<div id="sigma"><img src=DeltaPhivsEtaSigmaEtOverEt_vs_et.gif></div>
+<div id="thumb_dl"><a href="#" id="thumblink_l"><img src="" id="thumb_l" width=0 height=0 border=0></a></div>
+<div id="thumb_dr"><a href="#" id="thumblink_r"><img src="" id="thumb_r" width=0 height=0 border=0></a></div>
+</body>
+</html>
---
+++ test/ValidationFramework/WebScripts/main.php
@@ -0,0 +1,6 @@
+
+<a href=compatibility_deltaEtvsEt.php>compatibility_deltaEtvsEt.php</a><p>
+<a href=compatibility_deltaPhivsEta.php>compatibility_deltaPhivsEta.php</a><p>
+<a href=compatibility_deltaEtvsPhi.php>compatibility_deltaEtvsPhi.php</a><p>
+<a href=compatibility_deltaPhivsPhi.php>compatibility_deltaPhivsPhi.php</a><p>
+<a href=compatibility_deltaEtavsEta.php>compatibility_deltaEtavsEta.php</a><p>
