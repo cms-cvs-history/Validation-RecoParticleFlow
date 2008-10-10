d6ce45af5e8f1c4b3f797ed5542245538188cb2d
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2008-10-10 20:49:21 +0000

    organized python files. normalized the names. test file is now test/jetBenchmarks_cfg.py

---
+++ test/jetBenchmarks_cfg.py
@@ -0,0 +1,74 @@
+# Runs PFBenchmarkAnalyzer and PFJetBenchmark on PFJet sample to
+# monitor performance of PFJets
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("TEST")
+process.load("DQMServices.Core.DQM_cfg")
+
+
+process.load("RecoJets.Configuration.GenJetParticles_cff")
+process.load("RecoJets.Configuration.RecoGenJets_cff")
+process.load("RecoJets.Configuration.RecoPFJets_cff")
+process.load("PhysicsTools.HepMCCandAlgos.genParticles_cfi")
+process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
+
+
+process.source = cms.Source("PoolSource",
+fileNames = cms.untracked.vstring(
+'file:aod.root'
+)
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+
+process.load("Validation.RecoParticleFlow.pfJetBenchmark_cfi")
+process.load("Validation.RecoParticleFlow.caloJetBenchmark_cfi")
+
+
+process.p =cms.Path(
+#    process.genParticles*
+#    process.genJetParticles*
+#    process.recoGenJets*
+    process.pfJetBenchmark*
+    process.caloJetBenchmark
+    )
+
+
+process.schedule = cms.Schedule(process.p)
+
+
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.options = cms.untracked.PSet(
+    makeTriggerResults = cms.untracked.bool(True),
+    wantSummary = cms.untracked.bool(True),
+    Rethrow = cms.untracked.vstring('Unknown', 
+        'ProductNotFound', 
+        'DictionaryNotFound', 
+        'InsertFailure', 
+        'Configuration', 
+        'LogicError', 
+        'UnimplementedFeature', 
+        'InvalidReference', 
+        'NullPointerError', 
+        'NoProductSpecified', 
+        'EventTimeout', 
+        'EventCorruption', 
+        'ModuleFailure', 
+        'ScheduleExecutionFailure', 
+        'EventProcessorFailure', 
+        'FileInPathError', 
+        'FatalRootError', 
+        'NotFound')
+)
+
+process.MessageLogger.cerr.FwkReport.reportEvery = 50
+process.genParticlesForJets.ignoreParticleIDs.append(14)
+process.genParticlesForJets.ignoreParticleIDs.append(12)
+process.genParticlesForJets.ignoreParticleIDs.append(16)
+process.genParticlesForJets.excludeResonances = False
+
