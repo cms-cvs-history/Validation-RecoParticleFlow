fa207ca76981cafad37796cbc3f814f23aa0b66f
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2008-10-26 08:54:31 +0000

    more cleaning on the jet benchmark side

--- test/jetBenchmark_cfg.py
+++ test/jetBenchmark_cfg.py
@@ -12,32 +12,14 @@ fileNames = cms.untracked.vstring(
 'file:aod.root'
 )
 )
-
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
 )
 
-
-
-process.load("RecoJets.Configuration.GenJetParticles_cff")
-process.load("RecoJets.Configuration.RecoGenJets_cff")
-process.load("RecoJets.Configuration.RecoPFJets_cff")
-process.load("PhysicsTools.HepMCCandAlgos.genParticles_cfi")
-process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
-process.genParticlesForJets.ignoreParticleIDs.append(14)
-process.genParticlesForJets.ignoreParticleIDs.append(12)
-process.genParticlesForJets.ignoreParticleIDs.append(16)
-process.genParticlesForJets.excludeResonances = False
-
-process.load("Validation.RecoParticleFlow.pfJetBenchmarkGeneric_cfi")
-process.load("Validation.RecoParticleFlow.caloJetBenchmarkGeneric_cfi")
-
+process.load("Validation.RecoParticleFlow.jetBenchmarkGeneric_cff")
 
 process.p =cms.Path(
-    process.genJetParticles*
-    process.recoGenJets*
-    process.pfJetBenchmarkGeneric*
-    process.caloJetBenchmarkGeneric
+    process.jetBenchmarkGeneric
     )
 
 
