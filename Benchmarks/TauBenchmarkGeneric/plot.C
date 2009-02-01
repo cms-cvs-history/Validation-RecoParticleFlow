3244099e8258b88eca95b356d10998a53796da03
cmsbuild <>
2009-02-01 18:31:34 +0000

    This commit was manufactured by cvs2git to create tag 'Validation- RecoParticleFlow-V01-08-00'.
    Sprout from master 2008-11-24 12:13:03 UTC Patrick Janot <sha1-947b7d2151f410efe7b862ab9e58df9283c85909@cern.ch> 'Update to CMSSW220'
    Cherrypick from master 2009-02-01 18:31:33 UTC Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch> 'standardizing the name of the benchmark output file for tau benchmark. Should be done for the other benchmarks as well':
        Benchmarks/TauBenchmarkGeneric/captions.txt
        Benchmarks/TauBenchmarkGeneric/plot.C
        Benchmarks/Tools/indexGen.py
        Benchmarks/Tools/templates/index.html
        python/caloTauBenchmarkGeneric_cfi.py
        python/pfTauBenchmarkGeneric_cfi.py
    Delete:
        Benchmarks/METBenchmarkGeneric/benchmark_cfg.py
        Benchmarks/METBenchmarkGeneric/plot.C

--- Benchmarks/TauBenchmarkGeneric/plot.C
+++ Benchmarks/TauBenchmarkGeneric/plot.C
@@ -3,7 +3,7 @@
 gROOT->LoadMacro("../Tools/NicePlot.C");
 InitNicePlot();
 
-TFile f("tauBenchmarkGeneric.root");
+TFile f("benchmark.root");
 
 
 TCanvas c1;
@@ -13,6 +13,7 @@ f.cd("DQMData/PFTask/Benchmarks/iterativeCone5PFJets/Gen");
 TH1F* hpf = (TH1F*) gDirectory.Get("DeltaEt");
 hpf.Rebin(2);
 FormatHisto(hpf, s1);
+hpf->SetStats(1111);
 hpf.Draw();
 f.cd("DQMData/PFTask/Benchmarks/iterativeCone5CaloJets/Gen");
 TH1F* hcalo = (TH1F*) gDirectory.Get("DeltaEt");
