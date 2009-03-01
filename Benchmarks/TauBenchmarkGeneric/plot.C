0a10419d7a0895f1e28f4781c88b199f4aba1cd2
cmsbuild <>
2009-03-01 18:55:27 +0000

    This commit was manufactured by cvs2git to create tag 'Validation- RecoParticleFlow-validation_22X_2'.
    Sprout from master 2009-02-16 15:45:56 UTC Patrick Janot <sha1-947b7d2151f410efe7b862ab9e58df9283c85909@cern.ch> 'Minor fixes'
    Cherrypick from master 2009-03-01 18:55:26 UTC Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch> '- added stats again to NicePlot histo formatting':
        Benchmarks/TauBenchmarkGeneric/benchmark_cfg.py
        Benchmarks/TauBenchmarkGeneric/captions.txt
        Benchmarks/TauBenchmarkGeneric/plot.C
        Benchmarks/Tools/Comparator2.C
        Benchmarks/Tools/NicePlot.C
        Benchmarks/Tools/indexGen.py
        python/caloTauBenchmarkGeneric_cfi.py
        python/pfTauBenchmarkGeneric_cfi.py
    Delete:
        src/PFTauElecRejectionBenchmarkAnalyzer.cc

--- Benchmarks/TauBenchmarkGeneric/plot.C
+++ Benchmarks/TauBenchmarkGeneric/plot.C
@@ -1,26 +1,98 @@
 {
+  
+  gROOT->LoadMacro("../Tools/NicePlot.C");
+  InitNicePlot();
+  gROOT->LoadMacro("../Tools/Comparator2.C");
+  //  Manager::Init();
 
-gROOT->LoadMacro("../Tools/NicePlot.C");
-InitNicePlot();
+  gStyle->SetOptStat(1111);
 
-TFile f("benchmark.root");
+  Comparator comp("benchmark.root",
+		  "DQMData/PFTask/Benchmarks/iterativeCone5PFJets/Gen",
+		  "benchmark.root",
+		  "DQMData/PFTask/Benchmarks/iterativeCone5CaloJets/Gen");
+  comp.SetStyles(s1, s2, "Particle Flow Taus", "Calo Taus");
+
+  Comparator::Mode mode = Comparator::NORMAL;
 
 
-TCanvas c1;
-FormatPad( &c1, false );
+  comp.SetAxis(5);
 
-f.cd("DQMData/PFTask/Benchmarks/iterativeCone5PFJets/Gen");
-TH1F* hpf = (TH1F*) gDirectory.Get("DeltaEt");
-hpf.Rebin(2);
-FormatHisto(hpf, s1);
-hpf->SetStats(1111);
-hpf.Draw();
-f.cd("DQMData/PFTask/Benchmarks/iterativeCone5CaloJets/Gen");
-TH1F* hcalo = (TH1F*) gDirectory.Get("DeltaEt");
-hcalo.Rebin(2);
-FormatHisto(hcalo, s2);
-hcalo.Draw("same");
+  TCanvas c0("c0", "legend", 400, 200);
+  FormatPad( &c0, false ); 
+  comp.Legend().Draw();
+  gPad->SaveAs("legend.png");
+    
+  TCanvas c1a("c1a", "Tau benchmark, low pT");
+  FormatPad( &c1a, false ); 
+  comp.DrawSlice("DeltaEtOverEtvsEt", 10, 50, mode);
+  gPad->SaveAs("tauBenchmarkGeneric_lowpT.png");
 
-gPad->SaveAs("tauBenchmarkGeneric.png");
+  TCanvas c1b("c1b", "Tau benchmark, high pT");
+  FormatPad( &c1b, false );
+  comp.SetAxis(5);
+  comp.DrawSlice("DeltaEtOverEtvsEt", 400, 600, mode);
+  gPad->SaveAs("tauBenchmarkGeneric_highpT.png");
 
+  comp.SetAxis(1, -0.1,0.1);
+
+  TCanvas c2a("c2a", "Eta resolution, low pT");
+  FormatPad( &c2a, false );
+  comp.DrawSlice("DeltaEtavsEt", 10, 50, mode);
+  gPad->SaveAs("deltaEta_lowpT.png");
+
+  TCanvas c2b("c2b", "Eta resolution, high pT");
+  FormatPad( &c2b, false );
+  comp.DrawSlice("DeltaEtavsEt", 400, 600, mode);
+  gPad->SaveAs("deltaEta_highpT.png");
+
+
+  TCanvas c3a("c3a", "Phi resolution, low pT");
+  FormatPad( &c3a, false );
+  comp.DrawSlice("DeltaPhivsEt", 10, 50, mode);
+  gPad->SaveAs("deltaPhi_lowpT.png");
+
+  TCanvas c3b("c3b", "Phi resolution, high pT");
+  FormatPad( &c3b, false );
+  comp.DrawSlice("DeltaPhivsEt", 400, 600, mode);
+  gPad->SaveAs("deltaPhi_highpT.png");
+
+  comp.SetAxis(1);
+
+
+  bool efficiencies = false;
+
+  if (efficiencies ) {
+
+  TCanvas c4("c4", "Efficiency PF");
+  FormatPad( &c4, false );
+  unsigned rebin = 10;
+
+  Comparator comp2("benchmark.root",
+		   "DQMData/PFTask/Benchmarks/iterativeCone5PFJets/Gen",
+		   "benchmark.root",
+		   "DQMData/PFTask/Benchmarks/iterativeCone5PFJets/Gen" ); 
+  comp2.SetStyles(s1, s2);
+  comp2.Draw("EtSeen","EtGen", Comparator::EFF);
+  gPad->SaveAs("efficiency_vs_pT_pf.png");
+
+  TCanvas c5("c5", "Efficiency Calo");
+  FormatPad( &c5, false );
+
+  Comparator comp3("benchmark.root",
+		   "DQMData/PFTask/Benchmarks/iterativeCone5CaloJets/Gen",
+		   "benchmark.root",
+		   "DQMData/PFTask/Benchmarks/iterativeCone5CaloJets/Gen" ); 
+  comp3.SetStyles(s1, s2);
+  comp3.Draw("EtSeen","EtGen", Comparator::EFF);
+  gPad->SaveAs("efficiency_vs_pT_calo.png");
+}
+
+  TCanvas c6("c6", "pT");
+  FormatPad( &c6, false );
+
+  comp.Draw("EtGen", mode);
+  gPad->SaveAs("etgen.png");
+
+  
 }
